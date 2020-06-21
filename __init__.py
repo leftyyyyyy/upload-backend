f37cff39a4022f0488061855df49c9361e0071af
leftyyyyyy <bandzbond@gmail.com>
2020-06-21 22:48:12 +0000

    adding new requirements!

--- __init__.py
+++ __init__.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+from flask_session import Session
+import redis
 
 app     = Flask(__name__)
 app.config.from_object("config")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+print(app.config)
+
+@app.route("/fetchffiles", methods=['GET'])
+def fetch_files():
+    return 0
+
+@app.route("/download", methods=['GET'])
+def download():
+    """Check if the user has the file.
+    Retrieve file name.
+    Check if the file exists locally
+    If not fetch from S3
+    """
+
+    return 0
+
+@app.route("/upload", methods=['POST'])
+def upload():
+
+    return 0
 
 if __name__ == "__main__":
     app.run()

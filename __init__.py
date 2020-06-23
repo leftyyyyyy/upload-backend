95ffae21e382b1b6f14189d14219857929493333
leftyyyyyy <bandzbond@gmail.com>
2020-06-23 00:11:29 +0000

    download is done

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, make_response, request, jsonify
+from flask import Flask, render_template, session, redirect, make_response, request, jsonify, Response
 from flask_session import Session
 import redis
 from multiprocessing import Process, Queue
@@ -35,10 +35,10 @@ Redis client for storing and retrieving user/files information
 redisClient = redis.StrictRedis(host='127.0.0.1', port=6379)
 
 """
-Queues for file uploading/downloading
+Counters for file uploading/downloading
 """
-uploadQueue = Queue()
-downloadQueue = Queue()
+uploading = 0
+downloading = 0
 
 """
 Determine if the file is in an accepted format
@@ -153,57 +153,53 @@ If not fetch from S3
 @app.route("/download", methods=['GET'])
 def download_file():
 
+    print("A")
     """
     Ensure the session cookie exists in the request
     """
     try:
         user_id = request.cookies.get('session')
-        _input_file_identifier = request.args.get('file')
+        _input_file_identifier = request.args.get('file_identifier')
     except:
         return redirect("/")
 
+    print("B")
     """
     Ensure the session cookie exists in redis
     """   
     if not redisClient.get('session:{}'.format(user_id)):
         return redirect("/")
 
+    print("C")
     """
     Retrieve the file identifiers for the user
     """
     _file_identifiers = redisClient.lrange(user_id, 0, -1 )
 
+    print("D")
     """
     Check if the input identifier belongs to the user, if so fetch its name
     """
     for _id in _file_identifiers:
-        if _id == _input_file_identifier:
-            _file_name = redisClient.hgetall(_id)['Name']
+        if _id.decode("utf-8") == _input_file_identifier:
+            _file_name = redisClient.hgetall(_id)[b'Name'].decode("utf-8")
+            print("the requested file had the following name", _file_name)
 
     if not _file_name:
-        return False
+        return ("File not found", 404)
     
-    """
-    Check if the file exists locally first
-    """
-    _local_file_path = os.path.join(app.config['UPLOAD_FOLDER'], _input_file_identifier + '.csv')
-    if(os.path.isfile(_local_file_path)):
-        return send_file(_local_file_path, attachment_filename=_file_name)
+    _file_name_in_s3 = _input_file_identifier + ".csv"
 
-    """
-    If it doesn't exist locally, download from S3, return the file, then remove it
-    """  
-    #for i in range(redisClient.llen(user_id)):
-    #    if redisClient.lindex(user_id, i).decode("utf-8") == _file_identifier:
-
+    print(_file_name_in_s3)
+    
     return Response(
-        get_object(s3, total_bytes),
+        get_object(_file_name_in_s3),
         mimetype='text/plain',
         headers={"Content-Disposition": "attachment;filename=test.txt"}
     )
 	#return send_file('/var/www/PythonProgramming/PythonProgramming/static/images/python.jpg', attachment_filename='python.jpg')
 
-    return False
+    return ('Download successful',200)
 
 @app.route("/upload", methods=['POST'])
 def upload_file():
@@ -265,6 +261,8 @@ def upload_file():
     #         break
             
     #     upload_chunk_to_s3(_filename, chunk)
+
+    uploading-=1
 
     return ('Upload successful',200)
 
--- helpers.py
+++ helpers.py
@@ -24,15 +24,18 @@ def upload_file_to_s3(key, file_object, acl="public-read"):
     return True
 
 def get_total_bytes(file_name):
-    result = s3.list_objects(Bucket=bucket_name)
+    result = s3.list_objects(Bucket=S3_BUCKET)
     for item in result['Contents']:
         if item['Key'] == file_name:
             return item['Size']
 
-def get_object(file_name, total_bytes):
+def get_object(file_name):
+
+    total_bytes = get_total_bytes(file_name)
+
     if total_bytes > 1000000:
         return get_object_range(file_name, total_bytes)
-    return s3.get_object(Bucket=bucket_name, Key=file_name)['Body'].read()
+    return s3.get_object(Bucket=S3_BUCKET, Key=file_name)['Body'].read()
 
 def get_object_range(file_name, total_bytes):
     offset = 0
@@ -41,4 +44,4 @@ def get_object_range(file_name, total_bytes):
         total_bytes -= 1000000
         byte_range = 'bytes={offset}-{end}'.format(offset=offset, end=end)
         offset = end + 1 if not isinstance(end, str) else None
-        yield s3.get_object(Bucket=bucket_name, Key=file_name, Range=byte_range)['Body'].read()
+        yield s3.get_object(Bucket=S3_BUCKET, Key=file_name, Range=byte_range)['Body'].read()
